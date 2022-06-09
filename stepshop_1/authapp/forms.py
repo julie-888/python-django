87314ea61dbb72ebbacce6e5a797176abf8fefc2
Julie <julie.kondratuk@gmail.com>
2022-06-09 17:08:43 +0600

    add lesson_08 files

--- stepshop_1/authapp/forms.py
+++ stepshop_1/authapp/forms.py
@@ -1,5 +1,6 @@
-from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm, UserChangeForm
 from django import forms
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 from authapp.models import ShopUser
 
@@ -27,6 +28,59 @@ class ShopUserRegisterForm(UserCreationForm):
         )
     )
 
+    first_name = forms.CharField(
+        max_length=128,
+        widget=forms.TextInput(
+            attrs={
+                'placeholder': 'Имя',
+                'id': 'first_name',
+            }
+        )
+    )
+
+    password1 = forms.CharField(
+        strip=False,
+        widget=forms.PasswordInput(
+            attrs={
+                'id': 'password1',
+
+            }
+        )
+    )
+
+    age = forms.IntegerField(
+        validators=[MaxValueValidator(100),
+                    MinValueValidator(0)],
+        widget=forms.NumberInput(
+            attrs={
+                'placeholder': 'Возраст',
+                'id': 'age',
+                'min': 0,
+                'max': 100,
+            }
+        )
+    )
+
+    email = forms.EmailField(
+        widget=forms.EmailInput(
+            attrs={
+                'placeholder': 'Введите мэйл',
+                'id': 'email',
+
+            }
+        )
+    )
+
+    password2 = forms.CharField(
+        strip=False,
+        widget=forms.PasswordInput(
+            attrs={
+                'id': 'password2',
+
+            }
+        )
+    )
+
     class Meta:
         model = ShopUser
         fields = {'username', 'first_name', 'password1', 'password2', 'email', 'age', 'avatar'}
@@ -45,6 +99,31 @@ class ShopUserRegisterForm(UserCreationForm):
             raise forms.ValidationError('Вы слишком молоды!')
 
         return data
+
+
+class ShopUserEditForm(UserChangeForm):
+    class Meta:
+        model = ShopUser
+        fields = {'username', 'first_name', 'email', 'age', 'avatar', 'password'}
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control'
+            field.help_text = ''
+
+            if field_name == 'password':
+                field.widget = forms.HiddenInput()
+
+    def clean_age(self):
+        data = self.cleaned_data['age']
+
+        if data < 18:
+            raise forms.ValidationError('Вы слишком молоды!')
+
+        return data
+
 
 
 
